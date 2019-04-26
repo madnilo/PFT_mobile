628d251d9778e99c24227a40a55d693ac8ae58b6
Danilo Moura <danilomourag@gmail.com>
2019-04-26 18:25:29 +1200

    Fix permissions problems pointed by Goolge Play

--- screens/_utils/PermissionsHandler.js
+++ screens/_utils/PermissionsHandler.js
@@ -1,6 +1,6 @@
 export const checkCameraRollIOS = () => {
     const { Permissions } = Expo
-    const { status } = Permissions.getAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA)
-    if (!status) Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA)
+    const { status } = Permissions.getAsync(Permissions.CAMERA_ROLL)
+    if (!status) Permissions.askAsync(Permissions.CAMERA_ROLL)
 }
 
